5f05c23f85dcd883a972ed622a241520da03d7d6
Zach Schwartz <zachatorcodes@gmail.com>
2022-10-19 13:08:53 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+// Using a for loop.
+/*
+function writeCards(names, event) {
+    const writtenCards = [];
+    for (let i = 0; i < names.length; i++) {
+        writtenCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return writtenCards;
+}
+*/
+
+// Using a for of loop.
+function writeCards(names, event) {
+    const writtenCards = [];
+    for (let name of names) {
+        writtenCards.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return writtenCards;
+}
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num -= 1;
+    }
+}
